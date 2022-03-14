97c377032ed9f8a519beff26545ce1e5a06e28ab
Artur de Britto <arturbritto23@gmail.com>
2022-03-14 01:27:40 -0300

    Deploy

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,6 @@ import {ReactComponent as StarFull} from 'assets/img/star-full.svg';
 import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import './styles.css';
-import { type } from '@testing-library/user-event/dist/type';
 
 type Props = {
     score: number
